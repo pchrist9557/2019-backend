683123c9551ee5f8ead82772af7e5742015d5130
Christine Phung <34167591+pchrist9557@users.noreply.github.com>
2019-01-21 07:25:55 -0600

    Add files via upload

---
+++ purenodeserver.js
@@ -0,0 +1,32 @@
+const http = require ('http');
+const url = require('url');
+
+function handler(req, res) {
+
+    const parsedUrl = url.parse(req.url, true);
+    
+    if (parsedUrl.pathname === '/') {
+        res.writehead(200, {'Content-type':'text/plain'});
+        res.write('Hello, I am a webserver!');
+        res.end();
+    } else if (parsedUrl.pathname === '/time') {
+        res.writeHead (200,{'Content-type': 'text/plain'});
+        res.write(new Date(). toString());
+        return res.end();
+    } else if (parsedUrl.pathname === '/hello') {
+        const name = parsedUrl.query.name;
+        if(!name) {
+            res.writeHead (400, {'Content-type': 'text/plain'});
+            return res.end();
+        }
+    
+    } else {
+        res.writeHead (404., {'Content-type': 'text/plain'});
+        res.end();
+    }
+
+}
+
+const server = http.createServer(handler);
+
+server.listen(3000);
